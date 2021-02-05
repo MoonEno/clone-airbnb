1f63d5b3568b1c8d5dae6c0b1f0f1c64019472c2
ihmoon <ihmoon@openit.co.kr>
2021-02-05 11:29:45 +0900

    banner updates

--- src/component/content/Banner.jsx
+++ src/component/content/Banner.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../static/css/Banner.css';
 import { Button } from '@material-ui/core';
+import Search from '../common/Search'
 
 function Banner() {
+
+    const [showSearch, setShowSearch] = useState(false);
+    const [endDate, setEndDate] = useState( new Date());
+
+
     return (
         <div className="banner">
+            <div className="banner_search">
+                {showSearch && <Search />}
+                <Button onClick={() => setShowSearch(!showSearch)}
+                        className="banner_searchBtn" variant="outlined">Search Date</Button>
+            </div>
             <div className="banner_info">
                 <h1>IT IS Product TITLE </h1>
                 <h5>
